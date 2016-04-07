e947fabd7720dd683cd4c64ecce56e585e2c8368
z7059652 <7059652@163.com>
2016-04-07 12:19:06 +0800

    refine code of pycaffe

--- python/caffe/_caffe.cpp
+++ python/caffe/_caffe.cpp
@@ -7,7 +7,7 @@
 #include <boost/python.hpp>
 #include <boost/python/raw_function.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
-#include <numpy/arrayobject.h>
+#include "numpy/arrayobject.h"
 
 // these need to be included after boost on OS X
 #include <string>  // NOLINT(build/include_order)
